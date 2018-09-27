310f5ccec5ae3a6e9c7f2cbec2c420e4e6e8086e
Imanol <imanolgutierrez@yahoo.es>
2018-09-27 22:07:24 +0200

    Test para comprobar el video

--- downloader.py
+++ downloader.py
@@ -1,12 +1,13 @@
 from pytube import YouTube
-import moviepy.editor as mp
+
 
 video = raw_input("URL del video: ")
 yt = YouTube(video)
 title = yt.title
 print(title)
-print(yt.streams.filter(progressive = True, file_extension = "mp4").all())
-a = yt.streams.filter(progressive = True, file_extension = "mp4").all()
-a[0].download()
-clip = mp.VideoFileClip(title + ".mp4")
-clip.audio.write_audiofile(title + ".mp3")
+try:
+	print(yt.streams.filter(progressive = True, file_extension = "mp4").all())
+	a = yt.streams.filter(progressive = True, file_extension = "mp4").all()
+	a[0].download()
+except:
+	exit()
