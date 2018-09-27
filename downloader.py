3c21c178d91ce570f37368fb3e195bb00042c280
Imanol <imanolgutierrez@yahoo.es>
2018-09-27 17:26:24 +0200

    Funciona la descarga de video pero no la conversion de video a audio

--- downloader.py
+++ downloader.py
@@ -1,10 +1,12 @@
 from pytube import YouTube
-import os
+import moviepy.editor as mp
 
-#video = input("URL del video: ")
-yt = YouTube('https://www.youtube.com/watch?v=va-uhVt93hA')
+video = raw_input("URL del video: ")
+yt = YouTube(video)
 title = yt.title
 print(title)
 print(yt.streams.filter(progressive = True, file_extension = "mp4").all())
 a = yt.streams.filter(progressive = True, file_extension = "mp4").all()
 a[0].download()
+clip = mp.VideoFileClip(title + ".mp4")
+clip.audio.write_audiofile(title + ".mp3")
