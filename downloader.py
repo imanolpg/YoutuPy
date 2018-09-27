f70f9d947e3f56979583c4be0dc37ddc7c3d381e
Imanol <imanolgutierrez@yahoo.es>
2018-09-27 17:14:26 +0200

    Corregido el codigo y probando la calidad del video

--- downloader.py
+++ downloader.py
@@ -6,4 +6,5 @@ yt = YouTube('https://www.youtube.com/watch?v=va-uhVt93hA')
 title = yt.title
 print(title)
 print(yt.streams.filter(progressive = True, file_extension = "mp4").all())
-yt.streams.filter(progressive = True, file_extension = "mp4").all().download()
+a = yt.streams.filter(progressive = True, file_extension = "mp4").all()
+a[0].download()
