5dada6eb834929d5267415da8e42d9601f649c8e
Imanol <imanolgutierrez@yahoo.es>
2018-09-29 13:27:57 +0200

    El programa funciona perfectamente. Proyecto terminado

--- downloader.py
+++ downloader.py
@@ -1,14 +1,20 @@
 from pytube import YouTube
+from pytube.helpers import safe_filename
 import os
 
+os.system("figlet YoutuPy")
 videos = open("videos.txt").readlines()
+os.system("rm -f *.mp3")
+os.system("rm -f *.mp4")
 for line in videos:
 	video = line
 	yt = YouTube(video)
-	title = yt.title
+	title = safe_filename(yt.title)
 	print(title)
 	a = yt.streams.filter(progressive = True, file_extension = "mp4").all()
 	a[0].download()
-	title = title.replace(",", "")
-	comando = "ffmpeg -i \'" + title  + ".mp4\' -vn -ab 192k -acodec libmp3lame -ac 2 \'" + title + ".mp3\'"
+	comando = "ffmpeg -i \'" + title  + ".mp4\' \'" + title + ".mp3\' >/dev/null 2>&1"
 	os.system(comando)
+os.system("mv -f *.mp3 canciones")
+os.system("rm -f *.mp4")
+print("------Terminado------")
